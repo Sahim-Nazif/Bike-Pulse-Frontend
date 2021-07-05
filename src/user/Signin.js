9435971ac6ccf0a09246d47ccbe38bc85f24edb7
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-04 22:51:12 -0400

    updated user sign in page

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import Layout from '../core/Layout'
 import {signin} from '../auth/index'
+import {Redirect} from 'react-router-dom'
+
 
 const Signin = () => {
 
@@ -9,10 +11,11 @@ const Signin = () => {
         email:'',
         password:'',
         error:'',
-        success:false
+        loading:false,
+        redirectUserTo:false
 
     })
-    const { email,password, error, success}=values;
+    const { email,password, error, loading, redirectUserTo}=values;
     //the dynamic value can any input value from the form
     const handleChange=dynamicValue=>event=>{
 
@@ -23,20 +26,17 @@ const Signin = () => {
     const clickSubmit=event=>{
       
         event.preventDefault()
-        setValues({...values, error:false})
+        setValues({...values, error:false, loading:true})
         signin({ email, password})
                 .then(data=>{
                      if (data.error){
-                setValues({...values, error: data.error, success:false})
+                setValues({...values, error: data.error, loading:false})
                      }
             else{
                 setValues({
                     ...values,
-                    email:'',
-                    password:'',
-                    error:'',
-                    success:true
-                        
+                    redirectUserTo:true
+
                 })
             }
         
@@ -49,12 +49,12 @@ const Signin = () => {
             <form >
 
                 <div className='form-group'>
-                    <label className='text-muted'>Email *</label>
+                    <label className='text-muted'>Email </label>
                     <input onChange={handleChange('email')} type='email' className='form-control' value={email}/>
                 </div>
               
                 <div className='form-group'>
-                    <label className='text-muted'>Password *</label>
+                    <label className='text-muted'>Password </label>
                     <input onChange={handleChange('password')}type='password' className='form-control' value={password}/>
                 </div>
                 <button onClick={clickSubmit} className='btn btn-dark'>
@@ -71,24 +71,31 @@ const Signin = () => {
         
         </div>
     )
-    const showSuccess=()=>{
+    const showLoading=()=>{
         return(
-            <div className='alert alert-info' style={{display: success? '': 'none'}}>
-          
-
-        </div>
+             loading && (
+                 <div className='alert alert-info'>
+                     <h2>Loading...</h2>
+                 </div>
+             )
      )
   
+    }
+
+    const redirectUser=()=>{
+        if (redirectUserTo) {
+            return <Redirect to='/'/>
+        }
     }
     return (
         
         <Layout title='Login' description='Did you know that registered users have more more perks? login to discover.'
         className='container col-md-8 offset-md-2'>
         
-        {showSuccess()}
+        {showLoading()}
         {showError()}
         {signInForm()}
-
+        {redirectUser()}
         </Layout>
       
     )
