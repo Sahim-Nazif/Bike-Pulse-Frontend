bb7879c816f3a9f039b9b0c07b1fd2e5fc73f277
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-07 22:11:42 -0400

    filter bikes by price range

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,52 +1,89 @@
 import React, { useState, useEffect } from 'react'
 import Layout from './Layout'
 import Card from './Card'
-import { getCategories } from './apiCore'
+import { getCategories, getFilteredProducts } from './apiCore'
 import Checkbox from './Checkbox'
-import {prices} from './FixedPrices'
+import { prices } from './FixedPrices'
+import RadioBox from './RadioBox'
+
 const Shop = () => {
 
-    const [myFilters, setMyFilters]=useState({
-        filters:{category:[], price:[]}
+    const [myFilters, setMyFilters] = useState({
+        filters: { category: [], price: [] }
     })
-    const [categories, setCategories]=useState([])
-    const [error, setError]=useState(false)
+    const [categories, setCategories] = useState([])
+    const [error, setError] = useState(false)
+    const [limit, setLimit] = useState(6)
+    const [skip, setSkip] = useState(0)
+    const [filteredResults, setFilteredResults] = useState(0)
 
 
-    const init=()=>{
-        getCategories().then(data=>{
-            if (data.error){
-               setError(data.error)
+    const init = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                setError(data.error)
             } else {
                setCategories(data)
             }
         })
     }
-useEffect(()=>{
-    init()
-},[])
+    useEffect(() => {
+        init()
+        loadFilteredResults(skip, limit, myFilters)
+    }, [])
+    const loadFilteredResults=(newFilters)=>{
+       // console.log(newFilters)
+       getFilteredProducts(skip, limit, newFilters)
+       .then(data=>{
+           if (data.error) {
+               setError(data.error)
+           } else{
+               setFilteredResults(data)
+           }
+       })
+    }
+    const handleFilters = (filters, filterBy) => {
+        const newFilters = { ...myFilters }
+        newFilters.filters[filterBy] = filters
+        if (filterBy == 'price') {
+            let priceValues = handlePrice(filters)
+            newFilters.filters[filterBy] = priceValues
+        }
+        loadFilteredResults(myFilters.filters)
+        setMyFilters(newFilters)
+    }
 
-const handleFilters=(filters, filterBy)=>{
-    const newFilters={...myFilters}
-    newFilters.filters[filterBy]=filters
+    const handlePrice = value => {
+        const data=prices
+        let array=[]
+        for(let key in data){
+            if (data[key]._id=== parseInt(value)) {
+                array=data[key].array
+            }
+        }
+        return array
+    }
 
-    setMyFilters(newFilters)
-}
+ 
     return (
         <Layout title='Shop Bikes' description='Search and find books of your choice'
             className='container'>
 
             <div className='row'>
-        <div className='col-4'>
-            <h4>Filter By Categories</h4>
-            <ul>
-            <Checkbox categories={categories} handleFilters={filters=>handleFilters(filters, 'category')}/>
-            </ul>
-        
-        </div>
-        <div className='col-8'>
-          {JSON.stringify(myFilters)}
-        </div>
+                <div className='col-4'>
+                    <h4>Filter By Categories</h4>
+                    <ul>
+                        <Checkbox categories={categories} handleFilters={filters => handleFilters(filters, 'category')} />
+                    </ul>
+                    <h4>Filter By Price Range</h4>
+                    <div>
+
+                        <RadioBox prices={prices} handleFilters={filters => handleFilters(filters, 'price')} />
+                    </div>
+                </div>
+                <div className='col-8'>
+                    {JSON.stringify(filteredResults)}
+                </div>
             </div>
         </Layout>
     )
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -20,7 +20,32 @@ const getCategories=()=>{
         .catch(err=>console.log(err))
 }
 
+
+
+const getFilteredProducts=(skip, limit, filters={})=>{
+    const data={
+        limit, skip, filters
+    }
+    return  fetch(`http://localhost:8000/api/products/by/search`, {
+         method:'POST',
+         headers:{
+             Accept:'application/json',
+             'Content-Type':'application/json',
+         
+
+         },
+         body:JSON.stringify(data)
+     })
+     .then(response=>{
+         return response.json()
+     })
+     .catch(err=>{
+         console.log(err)
+     })
+ }
+
  module.exports={
     getProducts,
-    getCategories
+    getCategories,
+    getFilteredProducts
  }
