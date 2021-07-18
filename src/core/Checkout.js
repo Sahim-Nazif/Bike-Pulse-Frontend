e947f497ff602e58e1bdc33435a0f6af01cff2ce
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-18 19:02:44 -0400

    get BrainTreeClientToken created

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -1,10 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import Layout from './Layout'
-import { getProducts } from './apiCore'
+import { getBraintreeClientToken } from './apiCore'
 import { isAuthenticated } from '../auth'
 import {Link} from 'react-router-dom'
 
 const Checkout = ({products}) => {
+
+    const [data, setData]=useState({
+        success:false,
+        clientToken:null,
+        error:'',
+        instance:{},
+        address:''
+    })
+ 
+    const userID=isAuthenticated() && isAuthenticated().user._id
+    const token=isAuthenticated() && isAuthenticated().user.token
+    
+
 
     const getTotal=()=>{
         return products.reduce((currentValue, nextValue)=>{
@@ -25,7 +38,7 @@ const Checkout = ({products}) => {
     
     return (
         <div>
-            <h5>Total includes HST: ${getTotal()}</h5>
+            <h5 className='text-danger'>Total includes HST: ${getTotal()}</h5>
             {showCheckout()}
         </div>
     )
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -80,3 +80,20 @@ export const listRelated = productId => {
         })
         .catch(err => console.log(err));
 };
+
+export const getBraintreeClientToken = (userId, token) => {
+    return fetch(`http://localhost:8000/api/braintree/getToken/${userId}`, {
+        method: "GET",
+        headers:{
+            Accept:'application/json',
+            'Content-Type':'application/json',
+            Authorization:`Bearer ${token}`
+        
+
+        },
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
