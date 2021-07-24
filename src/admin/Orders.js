e00aedb7723d8b7c77a38eda876a2cfe0b924041
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-24 16:36:27 -0400

    show placed orders details

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -3,7 +3,7 @@ import Layout from '../core/Layout'
 import { isAuthenticated } from '../auth'
 import { Link } from 'react-router-dom'
 import {listOrders } from './apiAdmin'
-
+import moment from 'moment'
 
 
 const Orders = () => {
@@ -48,7 +48,29 @@ const Orders = () => {
            <div className='row'>
                <div className='col-md-8 offset-md-2'>
                 {showTotalOrderPlaced(orders)}
-                
+                {orders.map((orders, i)=>{
+
+                    return ( 
+                        <div className='mt-5' key={i} style={{borderBottom:'5px solid indigo'}}>
+
+                            <h3 className='mb-5'>
+                                <span className='bg-primary'> Order Id: {orders._id}</span>
+                            </h3>
+                            <ul className="list-group mb-2">
+                                <li className='list-group-item'>{orders.status}</li>
+                                <li className='list-group-item'>Transaction ID: {orders.transaction_id}</li>
+                                <li className='list-group-item'>Amount: ${orders.amount}</li>
+                                <li className='list-group-item'>Ordered By: {orders.user.firstName} {orders.user.lastName}</li>
+                                <li className='list-group-item'>Ordered placed: {moment(orders.createdAt).fromNow()}</li>
+                                <li className='list-group-item'>Shipping Address: {orders.address}</li>
+                                
+                            </ul>
+                            <h3 className='mt-4 mb-4 font-italic'> Total products in the orders
+                             {orders.products.length}</h3>
+                            </div>
+                        
+                    )
+                })}
                </div>
            
            </div>
