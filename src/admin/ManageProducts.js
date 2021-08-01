53fe02a46763f911b64f6bf50a87069b126c132c
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-08-01 09:22:36 -0400

    created componenet to manage products

--- src/admin/ManageProducts.js
+++ src/admin/ManageProducts.js
@@ -1,1 +1,24 @@
 
+import React from 'react'
+import Layout from '../core/Layout'
+import { isAuthenticated } from '../auth'
+import { Link } from 'react-router-dom'
+
+
+
+const ManageProducts = () => {
+    return (
+        <Layout title='Manage Products' description='As Admin of this application you have privilages to perform CRUD on all products'
+        className='container'>
+
+           <div className='row'>
+               <div className='col-md-8 offset-md-2'>
+              
+               </div>
+           
+           </div>
+       </Layout>
+    )
+}
+
+export default ManageProducts
